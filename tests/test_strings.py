020043fb247b142efedb656cebea311f8ef3eaa5
JB-Tellez <jb.tellez@gmail.com>
2022-12-05 10:22:16 -0800

    skips string tests

--- tests/test_strings.py
+++ tests/test_strings.py
@@ -1,29 +1,38 @@
+import pytest
 from exercises.strings import string_to_int_under_thousand_check
 
+@pytest.mark.skip("comment out to enable test")
 def test_upper_case():
     pass
 
 
+@pytest.mark.skip("comment out to enable test")
 def test_title_case():
     pass
 
 
+@pytest.mark.skip("comment out to enable test")
 def test_string_to_int():
     actual = string_to_int_under_thousand_check("500")
     expected = True
     assert actual == expected
 
+
+@pytest.mark.skip("comment out to enable test")
 def test_f_strings():
     pass
 
 
+@pytest.mark.skip("comment out to enable test")
 def test_nth_char():
     pass
 
 
+@pytest.mark.skip("comment out to enable test")
 def test_split():
     pass
 
 
+@pytest.mark.skip("comment out to enable test")
 def test_length():
     pass
